48c6ee51c63fe6cc4c5d506104ba5053237ddaef
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-11-10 15:21:15 +0300

    + patch 1011 ++ add HTMLTestRunner for test_7

--- test_7SecurType.py
+++ test_7SecurType.py
@@ -1,5 +1,7 @@
 import time
 import unittest
+import HTMLTestRunner
+
 global str
 
 from selenium import webdriver
@@ -7,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Firefox()
 driver.get("http://dev.eor.gosapi.ru/")
@@ -29,21 +30,21 @@ class ASeleniumAutoTest_1(unittest.TestCase):
         assert "500" not in driver.title  # проверка на 500/404 ошибку
         assert "404" not in driver.title
         _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'hidden-xs')))
-    '''
-    def test_003OpenAllPjct(self):
-        wait = WebDriverWait(driver, 10)
-        #time.sleep(3)
-        _ = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'i.entypo-menu')))
-        assert "ЭОР" in driver.title
-        menu = driver.find_element_by_css_selector("i.entypo-menu").click()
-        time.sleep(2)
-        allpj = driver.find_element_by_link_text("Все проекты").click()
-    def test_004Not500or404(self):
-        assert "500" not in driver.title  # проверка на 500/404 ошибку
-        assert "404" not in driver.title
-    '''
+
     def test_003Question(self):
         menu = driver.find_element_by_css_selector("i.entypo-menu").click()
         time.sleep(2)
         question = driver.find_element_by_link_text("Вопросы/Приоритеты").click()
         _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'navbar-header')))
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ASeleniumAutoTest_1))
+    # File
+    buf = open("at_for_login_pass_test.html", 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=buf,
+        title='ПРОВЕРКА КОРРЕКТНОСТИ ВВОДА ЛОГИНА И ПАРОЛЯ',
+        description='Отчет по тестированию'
+    )
+    runner.run(suite)
